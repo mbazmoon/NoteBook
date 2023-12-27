419737963c088e18a16026ae714d900f5a6877e4
mohammad <m.b.azmoon@gmail.com>
2023-12-27 14:45:00 +0330

    Refactor for support password encryption

--- src/main/java/com/azmoon/notebook/service/impl/UserServiceImpl.java
+++ src/main/java/com/azmoon/notebook/service/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,10 +29,12 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public User save(User user) {
         log.debug("save user with username:{} to DB", user.getUsername());
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
